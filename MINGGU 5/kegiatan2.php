c254d3a032a4576600e7ba4c96e20b3c2a774e9e
Aji Sampurno <58951602+Aji-Sampurno@users.noreply.github.com>
2021-03-12 14:24:54 +0700

    MINGGU 5

---
+++ MINGGU 5/kegiatan2.php
@@ -0,0 +1,16 @@
+<?php
+class miniCar implements Car {
+	
+	private $model;
+	
+	public function setModel($name)
+	{
+		$this -> model = $name;
+	}
+	
+	public function getModel()
+	{
+		return $this -> model;
+	}
+}
+?>
