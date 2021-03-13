54e6c8e46c1ada64f83d14767eebdcf35afca8d2
Aji Sampurno <58951602+Aji-Sampurno@users.noreply.github.com>
2021-03-13 14:18:45 +0700

    MINGGU 4

---
+++ MINGGU 4/tugas_3.php
@@ -0,0 +1,31 @@
+<?php 
+class Tablet{
+	public $merk;
+	public $camera;
+	public $memory;
+	
+	protected function getMerk()
+	{
+		return $this -> merk;
+	}
+}
+
+class Handphone extends Tablet{
+	
+	public function beli_handphone()
+	{
+		return 
+	}
+	
+	public function beli_handphone()
+	{
+		return 
+	}
+	
+	public function beli_handphone()
+	{
+		return 
+	}
+}
+
+?>
