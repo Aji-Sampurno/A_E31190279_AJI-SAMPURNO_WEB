7c02c6a799f87964355f103e7ae33f68cd7626f9
Aji Sampurno <58951602+Aji-Sampurno@users.noreply.github.com>
2021-03-29 10:59:04 +0700

    MINGGU_8

--- MINGGU_8/CI/application/controllers/Mahasiswa.php
+++ MINGGU_8/CI/application/controllers/Mahasiswa.php
@@ -10,17 +10,58 @@
 
 		public function index()
 		{
-			$data['Mahasiswa'] = [
-				["nama"=>"Kim Jefry","prodi"=>"MIF"],
-				["nama"=>"Ahmad Kurniawan","prodi"=>"TKK"],
-				["nama"=>"Budi Sudarsono","prodi"=>"TIF"]
-				];
+		//	$data['Mahasiswa'] = [
+			//	["nama"=>"Kim Jefry","prodi"=>"MIF"],
+			//	["nama"=>"Ahmad Kurniawan","prodi"=>"TKK"],
+			//	["nama"=>"Budi Sudarsono","prodi"=>"TIF"]
+			//	];
+			$data['Mahasiswa'] = $this -> Mahasiswa_model -> get_data();
 			$this -> load -> view('view_mahasiswa',$data);
 		}
 
 		public function profil()
 		{
 			echo"ini adalah method profil pada controller Mahasiswa";
+		}
+
+		public function prodi()
+		{
+			$data['Mahasiswa'] = [
+				["nama"=>"Kim Jefry","prodi"=>"MIF"],
+				["nama"=>"Ahmad Kurniawan","prodi"=>"TKK"],
+				["nama"=>"Budi Sudarsono","prodi"=>"TIF"]
+				];
+			$data['Prodi'] = [
+				["prodi"=>"MIF","keterangan"=>"Manajemen Informatika"],
+				["prodi"=>"TKK","keterangan"=>"Teknik Komputer"],
+				["prodi"=>"TIF","keterangan"=>"Teknik Informatika"]
+				];
+			$data['Angkatan'] = [
+				["angkatan"=>"2016"],
+				["angkatan"=>"2017"],
+				["angkatan"=>"2018"]
+				];
+			$this -> load -> view('view_mahasiswa',$data);
+		}
+
+		public function angkatan()
+		{
+			$data['Mahasiswa'] = [
+				["nama"=>"Kim Jefry","prodi"=>"MIF"],
+				["nama"=>"Ahmad Kurniawan","prodi"=>"TKK"],
+				["nama"=>"Budi Sudarsono","prodi"=>"TIF"]
+				];
+			$data['Prodi'] = [
+				["prodi"=>"MIF","keterangan"=>"Manajemen Informatika"],
+				["prodi"=>"TKK","keterangan"=>"Teknik Komputer"],
+				["prodi"=>"TIF","keterangan"=>"Teknik Informatika"]
+				];
+			$data['Angkatan'] = [
+				["angkatan"=>"2016"],
+				["angkatan"=>"2017"],
+				["angkatan"=>"2018"]
+				];
+			$this -> load -> view('view_mahasiswa',$data);
 		}
 	}
 ?>
--- MINGGU_8/CI/application/views/view_mahasiswa.php
+++ MINGGU_8/CI/application/views/view_mahasiswa.php
@@ -6,4 +6,18 @@
 		echo "Prodi :".$row['prodi'];
 		echo "<br/>";
 	}
+
+	foreach($Prodi as $row)
+	{
+		echo "Program Studi :".$row['prodi'];
+		echo "<br/>";
+		echo "Keterangan :".$row['keterangan'];
+		echo "<br/>";
+	}
+
+	foreach($Angkatan as $row)
+	{
+		echo "Angkatan :".$row['angkatan'];
+		echo "<br/>";
+	}
 ?>
